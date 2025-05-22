72ba8183619c5b3ab8d5b75e5424f0237c316327
AllenJame123 <allenjame1234@.com>
2025-05-22 10:59:51 +0400

    fix(vercel): remove deprecated Vercel adapter, server.js, and config; keep Netlify settings intact

--- remix-migration/remix.config.js
+++ remix-migration/remix.config.js
@@ -1,18 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 const isNetlify = process.env.NETLIFY === "true";
-const isVercel = !!process.env.VERCEL;
 
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
-  serverBuildTarget: isNetlify ? "netlify" : isVercel ? "vercel" : undefined,
-  server: isNetlify
-    ? "./netlify/functions/server.js"
-    : isVercel
-    ? "./server.js"
-    : undefined,
+  ...(isNetlify && {
+    serverBuildTarget: "netlify",
+    server: "./netlify/functions/server.js",
+  }),
   tailwind: true,
   // Remove obsolete future flags if you see warnings
 }; 
