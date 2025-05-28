406b6a34e29599566ee21038c8d7af50a4617f83
AllenJame123 <allenjame1234@.com>
2025-05-28 17:04:31 +0500

    chore: add comment to force Netlify to use latest server.js version

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,13 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createRequestHandler } from '@remix-run/netlify';
 
-// Create the request handler
-const handler = createRequestHandler({
+// Trivial change to force Netlify to use the latest version
+export const handler = createRequestHandler({
   build: require.resolve('../../build'),
   mode: process.env.NODE_ENV
 });
-
-// Export the Netlify function handler
-export const handler = async (event, context) => {
-  return handler(event, context);
-};
