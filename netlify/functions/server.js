8339a31d0fd3eafdc1f16a89e8398b935142791c
AllenJame123 <allenjame1234@.com>
2025-05-28 17:24:14 +0500

    fix: use absolute path for build directory in Netlify server function

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,9 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import { createRequestHandler } from '@remix-run/netlify';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Trivial change to force Netlify to use the latest version
 export const handler = createRequestHandler({
-  build: require.resolve('../../build'),
+  build: join(__dirname, '../../build'),
   mode: process.env.NODE_ENV
 });
