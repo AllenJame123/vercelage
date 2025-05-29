78b098ac95f08a978049e2e38e4d15ba7dc09339
AllenJame123 <allenjame1234@.com>
2025-05-29 16:07:22 +0500

    fix: resolve __dirname declaration conflict in server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,10 +2,10 @@ import { createRequestHandler } from '@remix-run/netlify';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const serverDir = dirname(fileURLToPath(import.meta.url));
 
 // Trivial change to force Netlify to use the latest version
 export const handler = createRequestHandler({
-  build: join(__dirname, '../../build'),
+  build: join(serverDir, '../../build'),
   mode: process.env.NODE_ENV
 });
